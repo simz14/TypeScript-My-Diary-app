78a0811393e78b91fb1c9f35f5ce1295214e594a
Simona Zozuľaková <szozulakova@gmail.com>
2022-10-21 17:27:52 +0200

    diary class/save method done

---
+++ DiaryApp/main.ts
@@ -0,0 +1,14 @@
+import { Diary } from "../src/models/Diary";
+import { DayRecord } from "../src/models/Record_sub";
+
+let diary = new Diary();
+
+let r1 = new DayRecord(1, "Hello", "This is first record");
+let r2 = new DayRecord(2, "Hello", "This is second record");
+let r3 = new DayRecord(3, "Hello", "This is third record");
+
+diary.addDayRecord(r1);
+diary.addDayRecord(r2);
+diary.addDayRecord(r3);
+
+diary.save();
